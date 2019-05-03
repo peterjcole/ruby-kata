90d4419440bcf82b34b1b870a4dd3c542d96c79d
Peter Cole <11048765+peterjcole@users.noreply.github.com>
2019-05-03 20:41:27 +0100

    add list and leave commands, refactor, add command line runner

--- shopping_cart.rb
+++ shopping_cart.rb
@@ -28,8 +28,6 @@
 # So that I am not left out of pocket
 # I would like to receive the correct change once I have completed my transaction
 
-# TODO: add a command-line runner
-
 require 'bigdecimal'
 
 
@@ -41,26 +39,47 @@ class Supermarket
   end
 
   def price(name)
-    format(retrieve(name).price)
+    for_sale(name) ? "#{name}: #{format(retrieve(name).price)}" : "#{name} is not for sale!"
   end
 
   def scan(name)
-    @scanned.push(retrieve(name))
+    for_sale(name) ? @scanned.push(retrieve(name)) : "#{name} is not for sale!"
   end
 
   def total()
-    "Number of items: #{@scanned.length}\nTotal: #{format(add_up())}"
+    "Number of items: #{@scanned.length}\nTotal: #{format(add_up)}"
   end
 
   def pay(amount_paid)
     @total_paid = @total_paid + BigDecimal(amount_paid, 2)
-    change = amount_paid - add_up
     if change >= 0
-      return "Your change: #{format(change)}"
+      return "Total cost: #{format(add_up)}. Amount paid: #{format(@total_paid)}. Your change: #{format(change)}"
     else
-      return "You haven't paid enough! Get back here!"
+      return "Total cost: #{format(add_up)}. Amount paid: #{format(@total_paid)}. You haven't paid enough! Get back here and pay #{format(0 - change)}!"
     end
     
+  end
+
+  def change
+    @total_paid - add_up
+  end
+
+  def list
+    @merchandise.reduce("Items for sale:") do |accumulator, item|
+      accumulator + "\n#{item.name}: #{format(item.price)}"
+    end
+  end
+
+  def settled
+    BigDecimal(@total_paid) - add_up >= 0
+  end
+
+  def leave
+    if settled
+      change > 0 ? "Thanks, here is your #{format(change)} change. See you next time!" : "Thanks, have a nice day!"
+    else
+      "You can't leave until you've paid!" 
+    end
   end
 
   private
@@ -74,11 +93,14 @@ class Supermarket
   end
 
   def add_up
-    @scanned.reduce(0) do |accumulator, item|
+    @scanned.reduce(BigDecimal(0, 2)) do |accumulator, item|
       accumulator + item.price
     end
   end
 
+  def for_sale(name)
+    !@merchandise.select { |item| item.name == name }.empty?
+  end
 end
 
 class Item
@@ -91,6 +113,34 @@ class Item
 
 end
 
+def go_shopping(items)
+  supermarket = Supermarket.new(items)
+  action = ""
+  until action == 'leave' && supermarket.settled
+    action = get_action
+    case action
+    when  'scan', 'price', 'pay'
+      argument = get_argument(action)
+      action_method = action.to_sym
+      puts supermarket.send(action_method, argument)
+    when 'total', 'list', 'leave'
+      puts supermarket.send(action.to_sym)
+
+    end
+  end
+end
+
+def get_action
+  puts "Enter an action\nAvailable actions: list, price, scan, total, pay, leave"
+  gets.chomp
+end
+
+def get_argument(action)
+  puts "Enter what you want to #{action}"
+  gets.chomp
+end
+
+
 test_merch = [
   Item.new("bread", 1.12),
   Item.new("milk", 1.78),
@@ -98,13 +148,4 @@ test_merch = [
   Item.new("champagne", 75.01)
 ]
 
-test_market = Supermarket.new(test_merch)
-
-puts test_market.price("bread")
-
-test_market.scan("bread")
-test_market.scan("milk")
-
-puts test_market.total
-
-puts test_market.pay(3.50)
+go_shopping(test_merch)
